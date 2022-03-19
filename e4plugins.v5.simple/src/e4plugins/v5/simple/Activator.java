fc44777b86d42cc545a55f5c57035693a9fde4cf
mohanavelp <mohanavelp@NIBLP1220.nousinfo.com>
2022-03-19 16:33:55 +0530

    feat: Initial commit

---
+++ e4plugins.v5.simple/src/e4plugins/v5/simple/Activator.java
@@ -0,0 +1,22 @@
+package e4plugins.v5.simple;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ envsetup.bat
@@ -0,0 +1,5 @@
+set path=C:\Programs\Java\OpenJDK11U_HotSpot\bin;%path%;
+set JAVA_HOME=C:\Programs\Java\OpenJDK11U_HotSpot
+cd C:\PMV\WS\ECWS\e4plugins.v5
+cmd C:\PMV\WS\ECWS\e4plugins.v5
+echo %path%
